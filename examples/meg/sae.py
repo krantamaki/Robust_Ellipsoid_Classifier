e769ee1bcf1f85f1fc99888790f1fec26d34b6d8
Kasper Rantamäki <kasper.rantamaki@gmail.com>
2023-05-28 18:17:34 +0300

    Updates to SAE

--- examples/meg/sae.py
+++ examples/meg/sae.py
@@ -1,35 +1,32 @@
 """
 Program for training the semantic autoencoder for finding the embeddings
 """
-import torch
-import torchvision
-import torchvision.transforms as transforms
+import random
+import matplotlib.pyplot as plt
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
+from sklearn.manifold import TSNE
 
-import numpy as np
-
-from config import *
 from tools import *
 
 
 # Define the encoder for SAE
-# The input should be a batch of MEG measurements and output
-# a batch of word2vec vectors
+# The input should be a batch of MEG measurements and output a batch of word2vec vectors
 class Encoder(nn.Module):
     def __init__(self):
 
         super(Encoder, self).__init__()
 
         self.in_layer = nn.Linear(n_sensors * n_timepoints, 1000)
-        self.hid_layer = nn.Linear(1000, 500)
+        self.hid_layer1 = nn.Linear(1000, 750)
+        self.hid_layer2 = nn.Linear(750, 500)
         self.out_layer = nn.Linear(500, word2vec_dim)
 
     def forward(self, x):
 
         x = F.relu(self.in_layer(x))
-        x = F.relu(self.hid_layer(x))
+        x = F.relu(self.hid_layer1(x))
+        x = F.relu(self.hid_layer2(x))
         x = self.out_layer(x)
 
         return x
@@ -41,20 +38,30 @@ class Decoder(nn.Module):
         super(Decoder, self).__init__()
 
         self.in_layer = nn.Linear(word2vec_dim, 500)
-        self.hid_layer = nn.Linear(500, 1000)
+        self.hid_layer1 = nn.Linear(500, 750)
+        self.hid_layer2 = nn.Linear(750, 1000)
         self.out_layer = nn.Linear(1000, n_sensors * n_timepoints)
 
     def forward(self, z):
 
         z = F.relu(self.in_layer(z))
-        z = F.relu(self.hid_layer(z))
+        z = F.relu(self.hid_layer1(z))
+        z = F.relu(self.hid_layer2(z))
         z = self.out_layer(z)
 
         return z
 
 
 skip_training = False  # Set this to true if you're only evaluating a model
+generate_dataset = True  # Set to false if dataset already exists
 device = torch.device('cpu')
+
+if generate_dataset:
+    form_dataset()
+
+X_train, y_train, X_test, y_test = load_dataset()
+
+word2vec_dict = load_word2vec()
 
 # Define the models
 encoder = Encoder()
@@ -63,13 +70,15 @@ encoder.to(device)
 decoder = Decoder()
 decoder.to(device)
 
+print("Starting training...\n")
+
 # Train the model
 if not skip_training:
 
     # Values for training size
-    dataset_size = -1  # Number of datapoints in the complete dataset
-    num_epochs = 3  # How many times the whole dataset is passed through the model
-    batch_size = 32  # Number of datapoints passed to the model in each iteration (defined in trainloader)
+    dataset_size = 53_000  # Number of datapoints in the complete dataset
+    num_epochs = 10  # How many times the whole dataset is passed through the model
+    batch_size = 32  # Number of datapoints passed to the model in each iteration
     num_iters = dataset_size // batch_size  # Total number of iterations per epoch
 
     # Required additional objects
@@ -80,9 +89,14 @@ if not skip_training:
     # Main loop
     for e in range(1, num_epochs + 1):
         loss = -1
+        last = 0
         for i in range(num_iters):
+            if i % 100 == 0 and i != 0:
+                # Print the training result
+                print(f"After {i} iterations the loss is: {loss}")
+
             # Get a new mini-batch
-            images, word2vecs, labels = next_batch(batch_size)
+            images, word2vecs, labels, last = next_batch(X_train, y_train, word2vec_dict, batch_size, last)
 
             # Training
             optimizer.zero_grad()
@@ -92,8 +106,6 @@ if not skip_training:
             loss.backward()
             optimizer.step()
 
-        # Print the training result
-        print(f"After {e} epoch(s) the loss is: {loss}")
 
 if not skip_training:
     torch.save(encoder.state_dict(), 'sae_encoder.pth')
@@ -107,12 +119,71 @@ if skip_training:
     decoder.eval()
 
 # Pass the training points through the model and save the results
-training_points = ...
+
+print(f"\nDoing a forward pass on the training points to find the embeddings...\n")
+
+train_embed = []
+train_label = []
+
+# Also gather some embeddings for visualizations
+labels_to_viz = []
+for cat in label_categories:
+    labels_to_viz.append(random.choice(label_categories[cat]))
+
+viz_dict = dict(zip(labels_to_viz, [[] for _ in range(len(labels_to_viz))]))
+
+for i in range(X_train.shape[0]):
+    point = X_train[i, :]
+    label = y_train[i]
+
+    embed = encoder(point)
+
+    if label in labels_to_viz and len(viz_dict[label]) < 100:
+        viz_dict[label].append(embed.detach().numpy())
+
+    train_embed.append(embed.detach().numpy())
+    train_label.append(label)
 
 # Pass the testing points through the model and save the results
-testing_points = ...
+
+print(f"Doing a forward pass on the testing points to find the embeddings...\n")
+
+test_embed = []
+test_label = []
+
+for i in range(X_test.shape[0]):
+    point = X_test[i, :]
+    label = y_test[i]
+
+    embed = encoder(point)
+
+    test_embed.append(embed.detach().numpy())
+    test_label.append(label)
+
+print("Saving the embeddings...")
+
+np.save("train_embeddings.npy", np.array(train_embed))
+np.save("train_embedding_labels.npy", np.array(train_label))
+np.save("test_embeddings.npy", np.array(test_embed))
+np.save("test_embedding_labels.npy", np.array(test_label))
 
 
+print("Visualizing a sample of the embeddings...")
 
+fig, ax = plt.subplots(1)
 
+all_embeddings = []
+for label, embeddings in viz_dict.items():
+    all_embeddings.append(np.array(embeddings))
+
+all_embeddings = np.concatenate(all_embeddings)
+all_embeddings = TSNE().fit_transform(all_embeddings)
+
+for i, label in enumerate(viz_dict):
+    ax.plot(all_embeddings[i * 100:(i + 1) * 100, 0],
+            all_embeddings[i * 100:(i + 1) * 100, 1],
+            '.', label=label)
+
+plt.legend()
+plt.show()
 
--- examples/meg/tools.py
+++ examples/meg/tools.py
@@ -1,38 +1,164 @@
 """
 Potentially useful helper functions
 """
+import os
+import random
 import torch
+import numpy as np
+from scipy.io import loadmat
+
+from config import *
 
 
-def load_data(path: str):
+def rescale(X, scaling_factor):
+    scaler = scaling_factor * np.ones((X.shape[1],))
+    return np.array([scaler * x for x in X])
+
+
+def form_dataset():
     """
-    Function for loading MEG data from wanted file and shaping it into 2D tensor
+    Function for forming the training and testing datasets and saving them in .npy format
 
-    Returns: data: torch.Tensor
+    Returns: Void
     """
-    pass
+    print("\nForming the dataset...\n")
+
+    # Randomly exclude one label from each category
+    excluded_labels = []
+    for cat in label_categories:
+        excluded_labels.append(random.choice(label_categories[cat]))
+
+    print(f"Excluded labels are: {', '.join(excluded_labels)}\n")
+
+    # Gather the data into dictionaries of form label -> list[np.ndarray]
+    train_dict = dict(zip(list(set(distinct_labels.copy()) - set(excluded_labels)),
+                          [[] for _ in range(len(distinct_labels) - len(excluded_labels))]))
+    test_dict = dict(zip(distinct_labels, [[] for _ in range(len(distinct_labels))]))
+
+    for sub in range(n_subjects):
+        subject_id = "sub-{:02d}".format(sub + 1)
+        print(f"Processing subject: {subject_id}")
+        if subject_id in excluded_subjects:
+            continue
+
+        data_path = f"{subject_path}/{subject_id}/epoch_data/{subject_id}_modality-{stimuli}_epoch_data.npy"
+        data = np.load(data_path)
+
+        # NOTE! The MEG data has shape (60, 18, 204, 40), where 60 is the number of words, 18 the number of reps, 204
+        # the number of gradiometers and 40 the number of measurements per sensor. These will be changed to shape
+        # (60, 18, 204 * 40)
+
+        # Go over the nouns
+        for i in range(0, n_words):
+            label = distinct_labels[i]
+
+            if label in excluded_labels:
+                reps = np.array([data[i][rep].flatten() for rep in range(0, n_reps)])
+                np.random.shuffle(reps)
+                test_dict[label].append(rescale(reps[2 * (n_reps // 3):], 10 ** 12))
+            else:
+                reps = np.array([data[i][rep].flatten() for rep in range(0, n_reps)])
+                np.random.shuffle(reps)
+                train_dict[label].append(rescale(reps[:2 * (n_reps // 3)], 10 ** 12))
+                test_dict[label].append(rescale(reps[2 * (n_reps // 3):], 10 ** 12))
+
+    print()
+
+    # We want to average together multiple repetitions for each datapoint
+    # This means that the points aren't necessarily independent, but at least
+    # they should be cleaner and this way we get more of them.
+    # The averaging is done randomly so that 5 reps are averaged to form 1 datapoint
+    # Number of final datapoints is set at 53000 for training set (1000 points per label)
+    # and 12000 for testing set (200 points per label)
+
+    X_train = []
+    y_train = []
+    X_test = []
+    y_test = []
+
+    train_ind = np.arange(0, 2 * (n_reps // 3) * (n_subjects - len(excluded_subjects)), 1)
+    test_ind = np.arange(0, (n_reps // 3) * (n_subjects - len(excluded_subjects)), 1)
+
+    for label in distinct_labels:
+        print(f"Generating datapoints for label: {label}")
+        if label in excluded_labels:
+            test_data = np.concatenate(test_dict[label])
+            for i in range(200):
+                test_batch = np.random.choice(len(test_ind), size=n_averaged, replace=False)
+                X_test.append(np.average(test_data[test_batch], axis=0))
+                y_test.append(label)
+        else:
+            test_data = np.concatenate(test_dict[label])
+            for i in range(200):
+                test_batch = np.random.choice(len(test_ind), size=n_averaged, replace=False)
+                X_test.append(np.average(test_data[test_batch], axis=0))
+                y_test.append(label)
+
+            train_data = np.concatenate(train_dict[label])
+            for i in range(1000):
+                train_batch = np.random.choice(len(train_ind), size=n_averaged, replace=False)
+                X_train.append(np.average(train_data[train_batch], axis=0))
+                y_train.append(label)
+
+    print("\nSaving data...\n")
+
+    np.save("train.npy", np.array(X_train))
+    np.save("train_labels.npy", np.array(y_train))
+    np.save("test.npy", np.array(X_test))
+    np.save("test_labels.npy", np.array(y_test))
 
 
-def next_batch(data: torch.Tensor, batch_size: int, last: int):
+def load_dataset():
+    """
+    Loads the dataset formed by form_dataset function and returns it as torch.Tensor
+
+    Returns: (X_train: torch.Tensor, y_train: list, X_test: torch.Tensor, y_test: list)
+    """
+    assert os.path.exists("train.npy")
+    assert os.path.exists("train_labels.npy")
+    assert os.path.exists("test.npy")
+    assert os.path.exists("test_labels.npy")
+
+    X_train = torch.Tensor(np.load("train.npy"))
+    y_train = np.load("train_labels.npy").tolist()
+    X_test = torch.Tensor(np.load("test.npy"))
+    y_test = np.load("test_labels.npy").tolist()
+
+    return X_train, y_train, X_test, y_test
+
+
+def load_word2vec():
+    """
+    Function for loading the word2vec vectors
+
+    Returns: word2vec_dict: dict[str, np.ndarray]
+    """
+    m_semantic = loadmat(word2vec_path)
+    S = m_semantic['vectors']
+
+    return dict(zip(distinct_labels, S))
+
+
+def next_batch(data: torch.Tensor, labels: list, word2vecs: dict, batch_size: int, last: int):
     """
     Function for iterating over a tensor in mini-batches
 
-    Returns: (mini_batch: torch.Tensor, new_last: int)
+    Returns: (mini_batch: torch.Tensor, batch_word2vec: torch.Tensor, batch_labels: list, new_last: int)
     """
-    n_points = data.shape[0]
     n_dims = data.shape[1]
-    ret = torch.zeros(n_points, n_dims)
+    mini_batch = torch.zeros(batch_size, n_dims)
+    batch_labels = []
+    batch_word2vec = torch.zeros(batch_size, word2vec_dim)
 
     for i0 in range(batch_size):
-        last = last + i0
-        if last == n_points:
+        if last == data.shape[0]:
             last = 0
 
-        ret[i0, :] = data[last, :]
+        mini_batch[i0, :] = data[last, :]
+        batch_word2vec[i0, :] = torch.Tensor(word2vecs[labels[last]])
+        batch_labels.append(labels[last])
 
-    return ret, last
+        last += 1
 
-
-def save_embeddings(data: torch.Tensor, labels):
-    pass
+    return mini_batch, batch_word2vec, batch_labels, last
 
